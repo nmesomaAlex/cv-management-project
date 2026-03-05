a81705b9895126c1cff59b4337bb8bd30481caf0
nmesomaAlex <nmesomaalex2026@gmail.com>
2026-03-05 10:00:48 -0800

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,73 @@
+# first_name = 'jamila'
+# surname ='Smith'
+
+# full_name = first_name.capitalize() + ' ' + surname
+# print(full_name)
+# print(len(first_name))
+# print(len(surname))
+# print(full_name.endswith('Smith'))
+
+
+# now we are learning working with numbers
+
+# addition = 10 +20
+# multiplication = 20 * 10
+# subtraction = 20 - 10
+# division = 20 / 10
+# modulo = 20 % 10
+
+# print(addition)
+# print(multiplication)
+# print(subtraction)
+# print(division)
+# print(modulo)
+
+# now we are learning while loops
+
+# number =0
+
+# while number <= 10:
+#    print(number)
+#    number = number + 1
+# else:
+#    print('while loop ended and value and value number is ' + str(number))
+
+# now we are learning functions
+
+# def check_age(age):
+#    if age < 18:
+#        print('oops not an adult')
+#    else:
+#        print('hooray you are an adult')
+# print('hello'.endswith('O'))
+# print('hello'.endswith('o'))
+
+# check_age(18)
+# check_age(17)
+# check_age(99)
+
+# now we are learning class and objects
+
+# class Person:
+#    def __init__(self, name, age):
+#        self.name = name
+#        self.age = age
+#    def walk(self):
+#        print(self.name + ' is walking...' )
+
+#    def speak(self):
+#        print('hello my name is ' + self.name + ' and i am ' + str(self.age) + ' years old')
+
+# john = Person('John', 18)
+# john.speak()
+# john.walk()
+
+# print('------------')
+
+# mariam = Person('Mariam', 17)
+# mariam.speak()
+# mariam.walk()
+
+
+# now we are doing word document
+
